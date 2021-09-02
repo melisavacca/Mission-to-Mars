5de4017d5d1887dc1fc95099ea107c4383d81083
Melisa Vacca <melisa.vacca@gmail.com>
2021-09-02 10:52:52 -0400

    adding deliverable 2

--- scraping.py
+++ scraping.py
@@ -20,7 +20,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     #Stop webdriver and return data
@@ -98,6 +99,27 @@ if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
 
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
 
+     #2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    test = browser.find_by_css("a.product-item img")
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(len(test)):
+        hemisphere = {}
+        browser.find_by_css("a.product-item img")[i].click()
+        links_found = browser.links.find_by_text('Sample').first
+        hemisphere["img_url"] = links_found["href"]
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        print(hemisphere)
+        #Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
+    return hemisphere_image_urls
+
+    
 
 
